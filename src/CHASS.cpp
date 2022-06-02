ca2bd1d1a948574634ef4b8434be2e0e79398c2b
sraje010 <sraje010@ucr.edu>
2022-06-02 11:30:16 -0700

    Fixed logic errors

--- src/CHASS.cpp
+++ src/CHASS.cpp
@@ -1,9 +1,8 @@
 #include "../header/CHASS.h"
 
-CHASS::CHASS(int m, int s, int e, const string& n) {
-    Player(m, s, e, n);
-    major = "CHASS";
-}
+CHASS::CHASS(int m, int s, int e, const string& n) 
+ : Player(m, s, e, n)
+{ major = "CHASS"; }
 
 void CHASS::intro() {
     cout << "Welcome to UCR CHASS " << this->getName() << ". I hope you are ready to embark on your journey as a Highlander!" << endl;
