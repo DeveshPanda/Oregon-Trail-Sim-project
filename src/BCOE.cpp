beab9b8f5aea2c6a6f9f5a1c94b7d4bdd86269f3
sraje010 <sraje010@ucr.edu>
2022-06-02 11:31:43 -0700

    Fixed logic errors

--- src/BCOE.cpp
+++ src/BCOE.cpp
@@ -1,9 +1,8 @@
 #include "../header/BCOE.h"
 
-BCOE::BCOE(int m, int s, int e, const string& n) {
-    Player(m, s, e, n);
-    major = "BCOE";
-}
+BCOE::BCOE(int m, int s, int e, const string& n) 
+ : Player(m, s, e, n)
+{ major = "BCOE"; }
 
 void BCOE::intro() {
     cout << "Welcome to UCR BCOE " << this->getName() << ". I hope you are ready to embark on your journey as a Highlander!" << endl;
--- src/CNAS.cpp
+++ src/CNAS.cpp
@@ -1,9 +1,8 @@
-#include "../header/CNAS.h"
+nclude "../header/CNAS.h"
 
-CNAS::CNAS(int m, int s, int e, const string& n) {
-    Player(m, s, e, n);
-    major = "CNAS";
-}
+CNAS::CNAS(int m, int s, int e, const string& n) 
+ : Player(m, s, e, n)
+{ major = "CNAS"; }
 
 void CNAS::intro() {
     cout << "Welcome to UCR CNAS " << this->getName() << ". I hope you are ready to embark on your journey as a Highlander!" << endl;
